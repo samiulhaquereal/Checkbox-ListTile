4409a3aeab3a4c7835543839b3a36e4eb0f440e8
B.M.Samiul Haque Real <samiulreal198@gmail.com>
2023-10-25 20:06:03 +0600

    1st

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  bool? _isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(
+      body: Center(
+        child: CheckboxListTile(
+          title: Text('Checkbox List Tile'),
+          value: _isChecked,
+          onChanged: (bool? newValue) {
+            setState(() {
+              _isChecked = newValue;
+            });
+          },
+          activeColor: Colors.orangeAccent,
+          checkColor: Colors.white,
+          tileColor: Colors.black12,
+          subtitle: Text('This is a subtitle'),
+          controlAffinity: ListTileControlAffinity.leading,
+          tristate: true,
+        ),
+      ),
+    ));
+  }
+}
